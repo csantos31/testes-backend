2d64151fdbcf98a14fa928b6cbab7a75893f1a5c
Cyntia Santos <cyntia.aparecida@hotmail.com>
2021-05-27 20:07:11 -0300

    formatando output

--- DistanceUtils.java
+++ DistanceUtils.java
@@ -27,6 +27,9 @@ public class DistanceUtils{
         //VETOR COM CALCULO DE DISTANCIAS
         this.calculaDistancias();
 
+        //ORDENACAO DO VETOR
+        this.quickSort(vetorDistancia,0,this.vetorDistancia.length-1);
+
         return this.shortestDistance;
     }
     
@@ -99,5 +102,34 @@ public class DistanceUtils{
             return true;
         }
         return false;
+    }
+
+    //ALGORITMOS DE ORDENACAO
+    public int partition(double A[], int p, int r){
+        int i, j;
+        double x=A[r];
+        i=p-1;
+
+        for(j=p;j<r;j++){
+            if(A[j]<x){
+                i=i+1;
+                double aux = A[i];
+                A[i]=A[j];
+                A[j]=aux;
+            }
+        }
+        i=i+1;
+        double aux=A[i]; 
+        A[i]=A[r];    
+        A[r]=aux;
+        return i;
+    }
+
+    public void quickSort(double A[], int p, int r){
+        if(p<r){
+            int q = partition(A,p,r);
+            quickSort(A,p,q-1);
+            quickSort(A,q+1,r);
+        }
     }
 }
--- ShortestDistanceTest.java
+++ ShortestDistanceTest.java
@@ -2,7 +2,9 @@ public class ShortestDistanceTest{
     public static void main(String[] args) {       
         ShortestDistance shortestDistance = new DistanceUtils().getShortestDistance();
         for(int i=0;i<shortestDistance.getVetorDistancia().length;i++){
-            System.out.println("Distancia: "+shortestDistance.getVetorDistancia()[i]);
+            if(!String.format("%.2f", shortestDistance.getVetorDistancia()[i]).equals("0,00")){
+                System.out.println("Distancia: "+String.format("%.2f", shortestDistance.getVetorDistancia()[i]));
+            }
         }
     }
 }
